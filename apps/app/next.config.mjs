10aff66b53b3b7f2ed5d56c4784eac717b99a344
Francisco Moretti <franciscoemoretti@gmail.com>
2024-11-17 10:03:26 +0000

    using turbo

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -1,19 +1,26 @@
 import "./src/env.mjs";
-import { withSentryConfig } from "@sentry/nextjs";
+// import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@v1/supabase"],
   experimental: {
     instrumentationHook: process.env.NODE_ENV === "production",
+    turbo: {
+      rules: {
+        // Add specific rules if needed for Sentry
+      },
+    },
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  silent: !process.env.CI,
-  telemetry: false,
-  widenClientFileUpload: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  tunnelRoute: "/monitoring",
-});
+export default nextConfig;
+// TODO: Sentry support for turbopack is WIP https://github.com/getsentry/sentry-javascript/issues/8105
+// export default withSentryConfig(nextConfig, {
+//   silent: !process.env.CI,
+//   telemetry: false,
+//   widenClientFileUpload: true,
+//   hideSourceMaps: true,
+//   disableLogger: true,
+//   tunnelRoute: "/monitoring",
+// });
